94221b53512e8475919212a6077d68e82a94f19f
azazel47 <49670061+azazel47@users.noreply.github.com>
2025-11-05 15:12:38 +0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, UploadFile, File, HTTPException, Query, Request
+from fastapi import FastAPI, APIRouter, UploadFile, File, HTTPException, Query, Request, Response
 from fastapi.responses import StreamingResponse, FileResponse, JSONResponse
 from starlette.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
